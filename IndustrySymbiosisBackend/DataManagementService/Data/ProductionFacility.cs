4cca4d0f3c910f53b98f7ea65236849e18e902f6
qubi7 <54663633+qubi7@users.noreply.github.com>
2022-08-29 12:59:24 +0200

    added a ProductionFacility Class

---
+++ IndustrySymbiosisBackend/DataManagementService/Data/ProductionFacility.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
+
+namespace DataManagementService.Data
+{
+    public class ProductionFacility
+    {
+        [JsonPropertyName("facilityName")]
+        public string facilityName { get; private set; }
+        public int enterpriseID { get; private set; }
+        public string facilityAddress { get; private set; }
+
+        public ProductionFacility(string facilityName, int enterpriseID, string facilityAddress)
+        {
+            this.facilityName = facilityName;
+            this.enterpriseID = enterpriseID;
+            this.facilityAddress = facilityAddress;
+        }
+
+        /// <summary>
+        /// Returns string as JSON
+        /// </summary>
+        /// <returns></returns>
+        // public override string ToString() => JsonSerializer.Serialize<>(this);
+    }
+}
