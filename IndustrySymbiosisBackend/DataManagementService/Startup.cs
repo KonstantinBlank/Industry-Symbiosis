29c34cf5cc38af09a1c8295553d8a5d3dbbad14b
qubi7 <54663633+qubi7@users.noreply.github.com>
2022-08-29 17:43:35 +0200

    added production facility service to startup

--- IndustrySymbiosisBackend/DataManagementService/Startup.cs
+++ IndustrySymbiosisBackend/DataManagementService/Startup.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataManagementService.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +26,7 @@ namespace DataManagementService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<ProductionFacilityProviderService>(); //Add the Service so that the Controller can grap it 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
