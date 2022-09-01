04e24d2e741b1f88a99951fb6a51177f7aeabff9
qubi7 <54663633+qubi7@users.noreply.github.com>
2022-09-01 20:15:02 +0200

    modified Production Facility Class

--- IndustrySymbiosisBackend/DataManagementService/Data/ProductionFacility.cs
+++ IndustrySymbiosisBackend/DataManagementService/Data/ProductionFacility.cs
@@ -8,27 +8,28 @@ namespace DataManagementService.Data
     {
         [JsonPropertyName("facilityName")]
         public int ProductionFacilityId { get; private set; }
-        public int EnterpriseId { get; private set; }
-        public string FacilityName { get; private set; }
         public int PostAddressId { get; private set; }
-        public string PostAddressRecord1 { get; private set; }
-        public string PostAddressRecord2 { get; private set; }
-        public string Postcode { get; private set; }
-        public string City { get; private set; }
-        public string Street { get; private set; }
-        public string HouseNumber { get; private set; }
+        public int EnterpriseId { get; private set; }
+        public string? FacilityName { get; private set; }
+        public string? PostAddressRecord1 { get; private set; }
+        public string? PostAddressRecord2 { get; private set; }
+        public string? Postcode { get; private set; }
+        public string? City { get; private set; }
+        public string? Street { get; private set; }
+        public string? HouseNumber { get; private set; }
 
-        public ProductionFacility(int productionFacilityId, int enterpriseId, string facilityName, string postAddressRecord1, string postAddressRecord2,  string street, string houseNumber, string postcode, string city)
+        public ProductionFacility(int productionFacilityId, int postAddressId, int enterpriseId, string? facilityName, string? postAddressRecord1, string? postAddressRecord2,  string? street, string? houseNumber, string? postcode, string? city)
         {
             ProductionFacilityId = productionFacilityId;
+            PostAddressId = postAddressId;
             EnterpriseId = enterpriseId;
-            FacilityName = facilityName ?? throw new ArgumentNullException(nameof(facilityName));
+            FacilityName = facilityName;
             PostAddressRecord1 = postAddressRecord1;
             PostAddressRecord2 = postAddressRecord2;
-            Street = street ?? throw new ArgumentNullException(nameof(street));
-            HouseNumber = houseNumber ?? throw new ArgumentNullException(nameof(houseNumber));
-            Postcode = postcode ?? throw new ArgumentNullException(nameof(postcode));
-            City = city ?? throw new ArgumentNullException(nameof(city));
+            Street = street;
+            HouseNumber = houseNumber;
+            Postcode = postcode;
+            City = city;
         }
 
         public ProductionFacility(int enterpriseId, string facilityName, string postAddressRecord1, string postAddressRecord2, string street, string houseNumber, string postcode, string city)
