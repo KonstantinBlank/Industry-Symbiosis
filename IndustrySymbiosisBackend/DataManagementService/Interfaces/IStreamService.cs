f8e4bb61913d5164e4361044db50d917b8d35305
konstantin <22549837+KonstantinBlank@users.noreply.github.com>
2022-09-07 09:16:12 +0200

    Stream + bug fixes - work in progress - not stable

---
+++ IndustrySymbiosisBackend/DataManagementService/Interfaces/IStreamService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DataManagementService.Interfaces
+{
+    public interface IStreamService
+    {
+        string Get(int productionLineProcessId);
+        string Create(int productionLineProcessId, bool isInput, int materialId, int energyId, int amount, int interval);
+        int Update(int id, int? productionLineProcessId, bool? isInput, int? materialId, int? energyId, int? amount, int? interval);
+    }
+}
