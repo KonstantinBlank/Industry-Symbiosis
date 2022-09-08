7644602b8a2c9e9e405b2e621b4b875fc495dafe
qubi7 <54663633+qubi7@users.noreply.github.com>
2022-09-08 11:31:08 +0200

    added Matching Service Interface

---
+++ IndustrySymbiosisBackend/MatchingService/Interfaces/IMatchingService.cs
@@ -0,0 +1,14 @@
+﻿namespace MatchingService.Interfaces
+{
+    public interface IMatchingService
+    {
+
+        string GetAvailableInputStreams(string enterpriseId);
+        string GetAvailableOutputStreams(string enterpriseId);
+        string GetMatchingInputStreams(int outputStreamId);
+        string GetMatchingOutputStreams(int inputStreamId);
+        int create(string PersonalStreamId, string PartnerStreamId);
+
+
+    }
+}
