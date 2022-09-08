b769d2126b026a6b2ab15566bb173c9f56e59b42
qubi7 <54663633+qubi7@users.noreply.github.com>
2022-09-08 11:35:18 +0200

    Renamed Matching Service to StreamMatching service bc of Naming collisions of package

---
+++ IndustrySymbiosisBackend/MatchingService/Interfaces/IStreamMatchingService.cs
@@ -0,0 +1,14 @@
+﻿namespace MatchingService.Interfaces
+{
+    public interface IStreamMatchingService
+    {
+
+        string GetAvailableInputStreams(int enterpriseId);
+        string GetAvailableOutputStreams(int enterpriseId);
+        string GetMatchingInputStreams(int outputStreamId);
+        string GetMatchingOutputStreams(int inputStreamId);
+        int create(int enterpriseId, bool selectedIsInput, string selectedStreamId, string requestedStreamId, float amount, float priceProposal, string comment);
+
+
+    }
+}
